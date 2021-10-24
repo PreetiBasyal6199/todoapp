fb295b1f46c69effb81c2e50aa39edd93a4d4bbb
Preeti <nanubasyal@gmail.com>
2021-10-24 16:01:13 -0400

    Sttings updated

--- todo/settings.py
+++ todo/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b60l2r7$lwn9x-saj$6=!!5deuh=fdnhvc*dp&rdg5-b5x*&(u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
